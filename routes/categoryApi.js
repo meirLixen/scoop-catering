99f5490f70da3549931770dde9fc0d15cee3f3c7
meir lixenbrg <m0527685598@gmail.com>
2022-12-10 21:06:51 +0200

    clean code

--- routes/categoryApi.js
+++ routes/categoryApi.js
@@ -1,89 +1,84 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-const Category = require('../models/Category')
-
+const Category = require("../models/Category");
 
 //api category
 
 // add category
-router.post('/category', async (req, res) => {
-    const category = new Category(req.body)
-    try {
-        const newCategory = await category.save(function (err, category) {
-            if (err) {
-                console.error(err)
-                return err;
-            }
-            res.json({ status: 201, category: newCategory })
-        })
-    } catch (err) {
-        res.json({ status: 500, error: err })
-    }
-})
-
+router.post("/category", async (req, res) => {
+  const category = new Category(req.body);
+  try {
+    const newCategory = await category.save(function (err, category) {
+      if (err) {
+        console.error(err);
+        return err;
+      }
+      res.json({ status: 201, category: newCategory });
+    });
+  } catch (err) {
+    res.json({ status: 500, error: err });
+  }
+});
 
 // find and update category by id
-router.post('/categories/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ["name", "products"];
-    const isValidOpreration = updates.every((update) => {
-        allowedUpdates.includes(update)
-    })
-    if (isValidOpreration) {
-        return res.status(404).send('invalid update')
+router.post("/categories/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "products"];
+  const isValidOpreration = updates.every((update) => {
+    allowedUpdates.includes(update);
+  });
+  if (isValidOpreration) {
+    return res.status(404).send("invalid update");
+  }
+  try {
+    const category = await Category.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!category) {
+      res.status(404).send("category not found");
     }
-    try {
-        const category = await Category.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        if (!category) {
-            res.status(404).send('category not found')
-        }
-        res.send(category)
-    } catch (err) {
-        console.error(err);
-    }
-})
+    res.send(category);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 // DELETE CATEGORY
 
-router.delete('/category/:id', async (req, res) => {
-    Category.findByIdAndDelete(req.params.id, (err, category) => {
-        if (err)
-            res.status(400).send(err)
-        res.status(200).send(category)
-    })
-})
-
+router.delete("/category/:id", async (req, res) => {
+  Category.findByIdAndDelete(req.params.id, (err, category) => {
+    if (err) res.status(400).send(err);
+    res.status(200).send(category);
+  });
+});
 
 // get all categories
-router.get('/categories', async (req, res) => {
-    Category.find()
+router.get("/categories", async (req, res) => {
+  Category.find()
     .populate({
-        path : 'products',
-        populate : {
-          path : 'priceList.amount'
-        }
-      })
-      .then(categories => {
-        //Category.find().then(categories => {
-    
-
-        if (!categories)
-          null  
-        res.send(categories);
+      path: "products",
+      populate: {
+        path: "priceList.amount",
+      },
     })
-        .catch((err) => {
-            res.status(500).send(err)
-        })
-})
-// get category by id
-router.get('/category/:id', async (req, res) => {
-    try {
-        const category = await Category.findById(req.params.id)
-        res.status(200).json({ message: "find category", mycategory: category })
+    .then((categories) => {
+      //Category.find().then(categories => {
 
-    }
-    catch (error) {
-        res.status(400).send("error")
-    }
-})
-module.exports = router
+      if (!categories) null;
+      res.send(categories);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+// get category by id
+router.get("/category/:id", async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    res.status(200).json({ message: "find category", mycategory: category });
+  } catch (error) {
+    res.status(400).send("error");
+  }
+});
+module.exports = router;
--- routes/emailApi.js
+++ routes/emailApi.js
@@ -1,163 +1,163 @@
-const router = require('express').Router()
-const Email = require('../models/Email')
-const nodemailer = require('nodemailer');
+const router = require("express").Router();
+const Email = require("../models/Email");
+const nodemailer = require("nodemailer");
 // API EMAIL:
 
 //add email
 
-router.post('/email', async (req, res) => {
-    const email = new Email(req.body)
-    try {
-        const newEmail = await email.save(function (err, email) {
-            if (err) {
-                console.error(err)
-                return err;
-            }
-            res.json({ status: 201, email: email })
-        })
-    } catch (err) {
-        res.json({ status: 500, error: err })
-    }
-})
+router.post("/email", async (req, res) => {
+  const email = new Email(req.body);
+  try {
+    const newEmail = await email.save(function (err, email) {
+      if (err) {
+        console.error(err);
+        return err;
+      }
+      res.json({ status: 201, email: email });
+    });
+  } catch (err) {
+    res.json({ status: 500, error: err });
+  }
+});
 
 // edit email
-router.post('/emails/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ["status"];
-    const isValidOpreration = updates.every((update) => {
-        allowedUpdates.includes(update)
-    })
-    if (isValidOpreration) {
-        return res.status(404).send('invalid update')
+router.post("/emails/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["status"];
+  const isValidOpreration = updates.every((update) => {
+    allowedUpdates.includes(update);
+  });
+  if (isValidOpreration) {
+    return res.status(404).send("invalid update");
+  }
+  try {
+    const email = await Email.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!email) {
+      res.status(404).send("email not found");
     }
-    try {
-        const email = await Email.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        if (!email) {
-            res.status(404).send('email not found')
-        }
-        res.send(email)
-    } catch (err) {
-        console.error(err);
-    }
-})
+    res.send(email);
+  } catch (err) {
+    console.error(err);
+  }
+});
 //delete email
-router.delete('/email/:id', async (req, res) => {
-    Email.findByIdAndDelete(req.params.id, (err, email) => {
-        if (err)
-            res.status(400).send(err)
-        res.status(200).send(email)
-    })
-})
-
+router.delete("/email/:id", async (req, res) => {
+  Email.findByIdAndDelete(req.params.id, (err, email) => {
+    if (err) res.status(400).send(err);
+    res.status(200).send(email);
+  });
+});
 
 // get all emails
-router.get('/emails', async (req, res) => {
-    Email.find().then(emails => {
-        if (!emails)
-        res.send(emails);
+router.get("/emails", async (req, res) => {
+  Email.find()
+    .then((emails) => {
+      if (!emails) res.send(emails);
     })
-        .catch((err) => {
-            res.status(500).send(err)
-        })
-})
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 // get email by id
-router.get('/email/:id', async (req, res) => {
-    const id = req.params.id
-    Email.findById(id).then((email) => {
-        if (!email) {
-            return res.status(404).send('email not found')
-        }
-        res.send(email)
+router.get("/email/:id", async (req, res) => {
+  const id = req.params.id;
+  Email.findById(id)
+    .then((email) => {
+      if (!email) {
+        return res.status(404).send("email not found");
+      }
+      res.send(email);
     })
-        .catch((err) => {
-            res.status(500).send(err)
-        })
-})
-router.post('/api/sendEmail', (req, res) => {
-    console.log("dddd");
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'mindysahar@gmail.com',
-            pass: '0583233770'
-        }
+    .catch((err) => {
+      res.status(500).send(err);
     });
-    //let transporter = nodemailer.createTransport('smtps://benchamo.scoop.catering%40gmail.com:0584505023@smtp.gmail.com');
-    let mailList = [
-        'shimonsahar@gmail.com',
-        'mindifr@gmail.com',
-        'mindysahar@gmail.com'
-    ]
-    let dataMail = req.body
-    mailList.forEach(function (to, i, array) {
-        let mailOptions = {
-            from: 'ScoopCatering 👥 <foo@blurdybloop.com>',
-            subject: dataMail.subject,
-            text: dataMail.text,
-            html: dataMail.html,
-            to: dataMail.to,
-            cc: "*******",
-            // bcc: mailList,//עותק מוסתר וב to:[]
-            // html: '<p>Click <a href="http://134.209.41.196' + '">here</a> to show more details in site</p>',
-            attachments: [
-                {
-                    filename: dataMail.filename,
-                    // filename: 'image1.jpg',
-                    path: dataMail.image,
-                    // path: "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
-                }
-            ]
-        };
-        mailOptions.to = to
-
-        let mail = transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                console.error(error);
-            } else {
-                console.info('Email sent: ' + info.response);
-            }
-        });
-    })
 });
-router.post('/sendEmail', (req, res) => {
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'mindysahar@gmail.com',
-            pass: '0583233770'
-        }
-    });
-
-    let dataMail = req.body
-
+router.post("/api/sendEmail", (req, res) => {
+  console.log("dddd");
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "mindysahar@gmail.com",
+      pass: "0583233770",
+    },
+  });
+  //let transporter = nodemailer.createTransport('smtps://benchamo.scoop.catering%40gmail.com:0584505023@smtp.gmail.com');
+  let mailList = [
+    "shimonsahar@gmail.com",
+    "mindifr@gmail.com",
+    "mindysahar@gmail.com",
+  ];
+  let dataMail = req.body;
+  mailList.forEach(function (to, i, array) {
     let mailOptions = {
-        from: 'ScoopCatering 👥 <foo@blurdybloop.com>',
-        subject: dataMail.subject,
-        text: dataMail.text,
-        html: dataMail.html,
-        to: dataMail.to,
-        cc: "*******",
-        // bcc: mailList,//עותק מוסתר וב to:[]
-        // html: '<p>Click <a href="http://134.209.41.196' + '">here</a> to show more details in site</p>',
-        // attachments: [
-        //     {
-        //         filename: dataMail.filename,
-        //         filename: 'image1.jpg',
-        //         path: dataMail.image,
-        //         path: "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
-        //     }
-        // ]
+      from: "ScoopCatering 👥 <foo@blurdybloop.com>",
+      subject: dataMail.subject,
+      text: dataMail.text,
+      html: dataMail.html,
+      to: dataMail.to,
+      cc: "*******",
+      // bcc: mailList,//עותק מוסתר וב to:[]
+      // html: '<p>Click <a href="http://134.209.41.196' + '">here</a> to show more details in site</p>',
+      attachments: [
+        {
+          filename: dataMail.filename,
+          // filename: 'image1.jpg',
+          path: dataMail.image,
+          // path: "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+        },
+      ],
     };
+    mailOptions.to = to;
 
-
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            throw error;
-        } else {
-            console.info('Email successfully sent!');
-        }
+    let mail = transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.info("Email sent: " + info.response);
+      }
     });
+  });
+});
+router.post("/sendEmail", (req, res) => {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "mindysahar@gmail.com",
+      pass: "0583233770",
+    },
+  });
 
+  let dataMail = req.body;
+
+  let mailOptions = {
+    from: "ScoopCatering 👥 <foo@blurdybloop.com>",
+    subject: dataMail.subject,
+    text: dataMail.text,
+    html: dataMail.html,
+    to: dataMail.to,
+    cc: "*******",
+    // bcc: mailList,//עותק מוסתר וב to:[]
+    // html: '<p>Click <a href="http://134.209.41.196' + '">here</a> to show more details in site</p>',
+    // attachments: [
+    //     {
+    //         filename: dataMail.filename,
+    //         filename: 'image1.jpg',
+    //         path: dataMail.image,
+    //         path: "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+    //     }
+    // ]
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      throw error;
+    } else {
+      console.info("Email successfully sent!");
+    }
+  });
 });
 
-module.exports = router
+module.exports = router;
