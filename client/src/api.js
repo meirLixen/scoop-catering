da803b323e7f9929df5f4b7d8a5ad1e7ca3861d6
mindyGit <mindysahar@gmail.com>
2022-12-27 20:29:40 +0200

    edit baseUrl

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "https://scoopcatering.co.il/",
-  // baseURL: "http://localhost:3001/api",
-  // withCredentials: true,
+   //baseURL: "https://scoopcatering.co.il/",
+  baseURL: "http://localhost:3001/api",
+  withCredentials: true,
 });
 
 export default api;
