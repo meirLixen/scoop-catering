9bbcdcb6463e377e69361e8a8e060a2d33077b6b
mindyGit <mindysahar@gmail.com>
2022-12-29 13:39:41 +0200

    update_29.12.2022

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "http://localhost:3001/api",
+   // baseURL: "http://localhost:3002/api",
    baseURL: "https://scoopcatering.co.il",
   withCredentials: true
 });
